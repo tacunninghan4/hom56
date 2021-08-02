ce483a7e8fe2b4db8dcb4d669fff5f9949973afb
Todd <andrewcunningham770@gmail.com>
2021-08-02 06:24:26 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,64 @@
+function addTogether(){
+  
+    const [arg1, arg2] = [...arguments];
+    }
+    
+    IF ( typeof arg1 === 'number') {
+      return undefined;
+    }
+    
+    if (arg2 === undefined){
+      return function();
+    }
+    
+    
+    if (typeof arg2 === 'number'){
+      return undefined;
+    }
+    
+    return arg1 + arg2;
+    
+    console.log(addTogether(2, 3));
+    
+    
+    person = function(firstAndLast){
+      this.getFullName = function(){
+        return '';
+      }
+    }
+    
+    bob = new person(Bob, Ross);
+    
+    bob.getFullName(); 
+    
+    
+    
+    person = function(firstAndLast){
+      this.getFullName = function(){
+        return '';
+    
+    
+    let first = firstAndLast.split('')[0];
+    let last   = firstAndLast.split('')[1];
+    
+    this.getFullName =function(){
+      return ' ${first}  ${last}';
+    };
+    
+    
+    this.getFullName = function(){
+    return first;
+    }
+    
+    
+    this.getFullName = function(){
+      return last;
+      }
+    
+    
+      bob = new person(Bob, Ross);
+    
+      console.log(bob.getLastName());
+      
+    
+      
